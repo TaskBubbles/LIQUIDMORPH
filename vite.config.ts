74191692e6218e2235cefa72f4e808138dd64263
TaskBubbles <taskbubblesapp@gmail.com>
2026-07-24 16:32:32 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    base: '/your-repo-name/', 
+    base: '/LIQUIDMORPH/', 
     const env = loadEnv(mode, '.', '');
     return {
       server: {
